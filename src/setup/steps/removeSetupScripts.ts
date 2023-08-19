85ff80ec03149b80cec81b566db2feb1c90a86f9
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2023-08-18 21:10:28 -0400

    Initial commit

---
+++ src/setup/steps/removeSetupScripts.ts
@@ -0,0 +1,17 @@
+import fs from "node:fs/promises";
+
+const globPaths = [
+	"./bin",
+	"./script",
+	"./src/hydrate",
+	"./src/setup",
+	"./src/shared",
+	".github/workflows/hydrate.yml",
+	".github/workflows/setup.yml",
+];
+
+export async function removeSetupScripts() {
+	for (const globPath of globPaths) {
+		await fs.rm(globPath, { force: true, recursive: true });
+	}
+}
