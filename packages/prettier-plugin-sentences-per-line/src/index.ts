8aa7083f86017561dd214610fca022db82aa4f59
michael faith <michaelfaith@users.noreply.github.com>
2025-12-11 05:39:45 -0600

    feat(prettier-sentences-per-line): add option to define abbreviations
    This change addresses a bug where new lines were triggered after abbreviations like `e.g.`.  In addition to covering well-known cases, there's a new option to provide your own custom options.  By providing `knownAbbreviations` you can either override or extend the default set of known abbreviations.  See the README for more information.

--- packages/prettier-plugin-sentences-per-line/src/index.ts
+++ packages/prettier-plugin-sentences-per-line/src/index.ts
@@ -1,9 +1,53 @@
 import type { RootContent } from "mdast";
-import type { AstPath, Printer } from "prettier";
+import type {
+	AstPath,
+	Options,
+	Printer,
+	StringArraySupportOption,
+} from "prettier";
 
 import * as markdown from "prettier/plugins/markdown";
 
-import { modifyNodeIfMultipleSentencesInLine } from "./modifications/modifyNodeIfMultipleSentencesInLine.ts";
+import { modifyNodeIfMultipleSentencesInLine } from "./modifications/modifyNodeIfMultipleSentencesInLine.js";
+
+const defaultKnownAbbreviations = [
+	"eg.",
+	"e.g.",
+	"etc.",
+	"ex.",
+	"ie.",
+	"i.e.",
+	"vs.",
+];
+
+function createKnownAbbreviations(options: Options): string[] {
+	const optionValue = options.knownAbbreviations;
+	if (Array.isArray(optionValue)) {
+		let customKnownAbbreviations = optionValue as string[];
+		if (optionValue.includes("DEFAULT")) {
+			customKnownAbbreviations = customKnownAbbreviations.filter(
+				(v) => v !== "DEFAULT",
+			);
+			customKnownAbbreviations = [
+				...customKnownAbbreviations,
+				...defaultKnownAbbreviations,
+			];
+		}
+		return customKnownAbbreviations;
+	}
+	return defaultKnownAbbreviations;
+}
+
+export const options = {
+	knownAbbreviations: {
+		array: true,
+		category: "Global",
+		default: [{ value: defaultKnownAbbreviations }],
+		description:
+			"An array of custom abbreviations to ignore when determining sentence boundaries.",
+		type: "string",
+	},
+} satisfies Record<string, StringArraySupportOption>;
 
 export const parsers = {
 	...markdown.parsers,
@@ -17,8 +61,10 @@ export const printers = {
 	mdast: {
 		...mdastPrinter,
 		print(path: AstPath<RootContent>, options, print, args) {
-			modifyNodeIfMultipleSentencesInLine(path);
+			modifyNodeIfMultipleSentencesInLine(path, {
+				knownAbbreviations: createKnownAbbreviations(options),
+			});
 			return mdastPrinter.print(path, options, print, args);
 		},
 	},
-} satisfies Record<string, Printer>;
+} satisfies Record<string, Printer<RootContent>>;
--- packages/prettier-plugin-sentences-per-line/src/modifications/modifyNodeIfMultipleSentencesInLine.ts
+++ packages/prettier-plugin-sentences-per-line/src/modifications/modifyNodeIfMultipleSentencesInLine.ts
@@ -3,40 +3,57 @@ import type { AstPath } from "prettier";
 
 import { insertNewlineAt } from "./insertNewlineAt.ts";
 
+export interface ModifyNodeOptions {
+	knownAbbreviations?: string[];
+}
+
 export function modifyNodeIfMultipleSentencesInLine(
 	path: AstPath<RootContent>,
+	options: ModifyNodeOptions = {},
 ) {
 	const node = path.node;
 	if (node.type === "blockquote") {
-		modifyBlockquoteNode(node);
+		modifyBlockquoteNode(node, options);
 	} else if (node.type === "paragraph") {
-		modifyParagraphNode(node, "\n");
+		modifyParagraphNode(node, "\n", options);
 	}
 }
 
-function modifyBlockquoteNode(node: Blockquote) {
+function modifyBlockquoteNode(node: Blockquote, options: ModifyNodeOptions) {
 	for (const paragraph of node.children) {
 		if (paragraph.type === "paragraph") {
-			modifyParagraphNode(paragraph, "> ");
+			modifyParagraphNode(paragraph, "> ", options);
 		}
 	}
 }
 
-function modifyParagraphNode(node: Paragraph, insertion: string) {
+function modifyParagraphNode(
+	node: Paragraph,
+	insertion: string,
+	options: ModifyNodeOptions,
+) {
 	for (const child of node.children) {
 		if (child.type === "sentence") {
-			modifySentenceNode(child, insertion);
+			modifySentenceNode(child, insertion, options);
 		}
 	}
 }
 
-function modifySentenceNode(sentence: SentenceNode, insertion: string) {
+function modifySentenceNode(
+	sentence: SentenceNode,
+	insertion: string,
+	{ knownAbbreviations = [] }: ModifyNodeOptions,
+) {
 	for (let i = 0; i < sentence.children.length - 1; i++) {
 		const child = sentence.children[i];
 		if (
 			child.type === "word" &&
 			child.value.endsWith(".") &&
-			!/^\s*\d+\./.test(child.value)
+			// Skip any starting list number, e.g. "1. " or " 1. "
+			!/^\s*\d+\./.test(child.value) &&
+			!knownAbbreviations.some((word) =>
+				child.value.toLowerCase().endsWith(word.toLowerCase()),
+			)
 		) {
 			insertNewlineAt(sentence.children, i, insertion);
 		}
