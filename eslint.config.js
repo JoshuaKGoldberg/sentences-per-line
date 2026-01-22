3185c3a4771588ff9a977cfe4276d725ccc7d609
michael faith <michaelfaith@users.noreply.github.com>
2026-01-21 18:10:01 -0600

    fix yml keys

--- eslint.config.js
+++ eslint.config.js
@@ -101,13 +101,6 @@ export default defineConfig(
 		files: ["**/*.{yml,yaml}"],
 		rules: {
 			"yml/file-extension": ["error", { extension: "yml" }],
-			"yml/sort-keys": [
-				"error",
-				{
-					order: { type: "asc" },
-					pathPattern: "^.*$",
-				},
-			],
 			"yml/sort-sequence-values": [
 				"error",
 				{
