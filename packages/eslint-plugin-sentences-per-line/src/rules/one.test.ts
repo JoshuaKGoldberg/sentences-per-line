6c78cf57c98cb4dbe950cee1dd59834dc73fe27d
Phi Nguyen <64292779+AzineZ@users.noreply.github.com>
2026-06-27 13:15:49 -0700

    fix: detect sentences ending with ! and ? (#1175)
    <!-- 👋 Hi, thanks for sending a PR to sentences-per-line! 💖.
    Please fill out all fields below and make sure each item is true and [x]
    checked.
    Otherwise we may not be able to review your PR. -->
    
    ## PR Checklist
    
    - [x] Addresses an existing open issue: Does not detect sentences ending
    with ! or ? #22
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/sentences-per-line/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/sentences-per-line/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    <!-- Description of what is changed and how the code change does that.
    -->
    
    fixes #22 
    
    - Added to getIndexBeforeSecondSentence.ts the functionality to detect
    exclamation mark and question mark. It was initially handling just the
    period.
    - Modified modifyNodeIfMultipleSentencesInLine.ts so the function can
    walk the sentence node containing the exclamation mark and question
    mark.
    - Added tests for exclamation mark and question mark across test files
    in different plugins.

--- packages/eslint-plugin-sentences-per-line/src/rules/one.test.ts
+++ packages/eslint-plugin-sentences-per-line/src/rules/one.test.ts
@@ -42,6 +42,71 @@ Abc.
 Def.
 `,
 		},
+		{
+			code: "Hello! World.",
+			errors: [
+				{
+					column: 7,
+					endColumn: 8,
+					endLine: 1,
+					line: 1,
+					messageId: "multiple",
+				},
+			],
+			output: "Hello! \nWorld.",
+		},
+		{
+			code: "Hello? World.",
+			errors: [
+				{
+					column: 7,
+					endColumn: 8,
+					endLine: 1,
+					line: 1,
+					messageId: "multiple",
+				},
+			],
+			output: "Hello? \nWorld.",
+		},
+		{
+			code: "Really?! Wow.",
+			errors: [
+				{
+					column: 9,
+					endColumn: 10,
+					endLine: 1,
+					line: 1,
+					messageId: "multiple",
+				},
+			],
+			output: "Really?! \nWow.",
+		},
+		{
+			code: "Hello! World! Again.",
+			errors: [
+				{
+					column: 7,
+					endColumn: 8,
+					endLine: 1,
+					line: 1,
+					messageId: "multiple",
+				},
+			],
+			output: "Hello! \nWorld! Again.",
+		},
+		{
+			code: "1. Hello! World.",
+			errors: [
+				{
+					column: 10,
+					endColumn: 11,
+					endLine: 1,
+					line: 1,
+					messageId: "multiple",
+				},
+			],
+			output: "1. Hello! \nWorld.",
+		},
 	],
 	valid: [
 		"",
@@ -65,5 +130,11 @@ Abc. Def.
 Abc.
 Def.
 `,
+		"Hello! world.",
+		"Hello? world.",
+		"`Hello!` World.",
+		"`Hello?` World.",
+		"Hello!",
+		"Hello?",
 	],
 });
